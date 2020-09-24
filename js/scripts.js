935e837d3fe8cfe0b24eaa0d6e63c75eae89d5ca
Dennis HG <dennishg32@gmail.com>
2020-09-24 12:34:05 +0200

    creation of files js and css plus html file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var add = function(number1, number2) {
+    return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter first number: "));
+var number2 = parseInt(prompt("Enter second number: "));
+alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+    return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter first number: "));
+var number2 = parseInt(prompt("Enter second number: "));
+alert(subtract(number1, number2));
